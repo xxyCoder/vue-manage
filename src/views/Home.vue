ddbc8e73fe5d99093fd5c8e2be9c527f7bbd2681
xxy <1696944145@qq.com>
2023-04-03 21:09:55 +0800

    首页完善

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,20 +14,34 @@
                     <p>上次登录地点<span>北京</span></p>
                 </div>
             </el-card>
-            <el-card style="margin-top:20px;">
+            <el-card shadow="always" style="margin-top:20px;">
                 <el-table :data="state.tableData" style="width: 100%">
-                    <el-table-column v-for="(val,key) in state.tableLable" :prop="key" :label="val" :key="key"/>
+                    <el-table-column v-for="(val, key) in state.tableLable" :prop="key" :label="val" :key="key" />
                 </el-table>
             </el-card>
         </el-col>
         <el-col :span="16">
-            <div class="grid-content ep-bg-purple-light" />
+            <div class="num">
+                <el-card :body-style="{ display: 'flex',padding: '0' }" v-for="item in state.countData" :key="item.name">
+                    <el-icon :style="{ background: item.color }">
+                        <CircleCheck />
+                    </el-icon>
+                    <div class="detail">
+                        <p class="price">￥{{ item.value }}</p>
+                        <p class="desc">{{ item.name }}</p>
+                    </div>
+                </el-card>
+            </div>
         </el-col>
     </el-row>
 </template>
 
 <script setup>
 import { reactive } from 'vue'
+import {
+    CircleCheck
+} from '@element-plus/icons-vue';
+
 const state = reactive({
     tableData: [
         {
@@ -60,7 +74,39 @@ const state = reactive({
         todayBuy: '今日购买',
         monthBuy: '本月购买',
         totalBuy: '总购买'
-    }
+    },
+    countData: [
+        {
+            name: '今日支付订单',
+            value: 1234,
+            color: '#2ec7c9'
+        },
+        {
+            name: '今日收藏订单',
+            value: 210,
+            color: '#ffb890'
+        },
+        {
+            name: '今日支付订单',
+            value: 1234,
+            color: '#5ab1ef'
+        },
+        {
+            name: '今日支付订单',
+            value: 1234,
+            color: '#2ec7c9'
+        },
+        {
+            name: '今日支付订单',
+            value: 1234,
+            color: '#ffb890'
+        },
+        {
+            name: '今日支付订单',
+            value: 1234,
+            color: '#5ab1ef'
+        },
+    ]
 })
 
 </script>
@@ -104,4 +150,41 @@ const state = reactive({
         }
     }
 }
-</style>
+
+.num {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    .el-card {
+        margin-bottom: 20px;
+        width: 32%;
+        .el-icon {
+            width: 80px;
+            height: 80px;
+            font-size: 30px;
+            text-align: center;
+            line-height: 80px;
+            color: #fff;
+        }
+        .detail {
+            display: flex;
+            margin-left: 15px;
+            flex-direction: column;
+            justify-content: center;
+
+            .price {
+                height: 30px;
+                line-height: 30px;
+                font-size: 30px;
+                margin-bottom: 10px;
+            }
+            .desc {
+                font-size: 14px;
+                color: #999;
+                text-align: center;
+            }
+        }
+    }
+
+}</style>
